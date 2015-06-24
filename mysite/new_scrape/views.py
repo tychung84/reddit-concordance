5c1de92e62264549022116f8c99cd0577637d315
Timothy Chung <timothychung@Timothys-MBP.fios-router.home>
2015-06-24 12:04:03 -0400

    small changes

--- mysite/new_scrape/views.py
+++ mysite/new_scrape/views.py
@@ -17,28 +17,35 @@ newComment = Base.classes.new_scrape_newcomment
 def processBody(text):
    openBrackets = '{(['
    closeBrackets = '}])'
-   punctuation = '.!?'   
+   punctuation = '.!?'
+   quotations = '"'   
 
    myBody = text.replace('\n','<br>')
    newString = myBody[0]
    checkCounter = 0
    counter = 1
    bracketCounter = 0
+   quoteCounter = 0
             
    while (checkCounter < 2) & (counter < len(myBody)):
          newString += myBody[counter]
-         if myBody[counter] in openBrackets:
+         if myBody[counter] in quotations:
+	    quoteCounter = (quoteCounter + 1) % 2
+         elif myBody[counter] in openBrackets:
             bracketCounter += 1
          elif myBody[counter] in closeBrackets:
             bracketCounter = max(bracketCounter - 1, 0)
-         elif (myBody[counter] in punctuation) & (bracketCounter == 0) & (myBody[counter-1] not in punctuation):
+         elif (myBody[counter] in punctuation) & (quoteCounter == 0) & (bracketCounter == 0) & (myBody[counter-1] not in punctuation):
             checkCounter += 1
          counter += 1
-   
-   return newString
+  
+   if quoteCounter == 1:
+	 return newString + '"'
+   else:
+   	 return newString
 
 def index(request):
-    myQuery = session.query(newComment).filter(newComment.score_ratio > 0.7)
+    myQuery = session.query(newComment).filter(newComment.body != '[deleted]').filter(~newComment.body.like('%https://%')).filter(newComment.score_ratio > 0.7).filter(newComment.current_score >= 20)
     totalCount = random.randint(0, myQuery.count())
     selectItem = myQuery.slice(totalCount, totalCount + 1).all()[0]
     
